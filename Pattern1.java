27d1e93b76e6fed2ba64672b711132ed2e4e8508
AnuAnu0641 <104294736+AnuAnu0641@users.noreply.github.com>
2022-05-09 19:06:08 +0530

    Testing my first commit
    Patterns code

---
+++ Pattern1.java
@@ -0,0 +1,20 @@
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i , j, count = 1;
+		for(i = 1; i<=3; i++)
+		{
+			for(j=1; j<=3; j++)
+			{
+			System.out.print(count + " ");
+			count++;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
