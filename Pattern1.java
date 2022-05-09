8388dfe44fc28d4a2d0610ea91a1c66c4fef077e
AnuAnu0641 <104294736+AnuAnu0641@users.noreply.github.com>
2022-05-09 19:13:32 +0530

    Added one comment and uploading again.

--- Pattern1.java
+++ Pattern1.java
@@ -3,7 +3,7 @@ public class Pattern1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		// added one extra comment.
 		int i , j, count = 1;
 		for(i = 1; i<=3; i++)
 		{
